f83350bc9260c70347923fa240473d8289575c3b
Rocio Avila <rocio.avila@northwestern.com>
2020-08-18 22:45:00 -0500

    Used index.html code for beersearch.handlebars

--- public/js/signup.js
+++ public/js/signup.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
     })
       .then(function(data) {
         // window.location.replace("/members");
-        window.location.replace("/beersearch.html")
+        window.location.replace("/beersearch")
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
