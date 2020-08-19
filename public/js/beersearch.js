b49add6f26e1b8b94a6be9303594c17a33585516
Rocio Avila <rocio.avila@northwestern.com>
2020-08-18 23:10:55 -0500

    login trouble shooting

--- public/js/beersearch.js
+++ public/js/beersearch.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
     
-    // INSERT JAVASCRIPT FOR BEERSEARCH.HTML HERE!
+        
+        $("#searchBtn").on("click", function(event) {
+            event.preventDefault();
+            var searchTerm = $("#search-term").val();
+            var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + searchTerm;
+            $.ajax({
+                url: queryURL,
+                method: 'GET'
+            }).then (function(result) {
+                console.log(result);
+                  const list = $("<ol>");
+                  for (let i = 0; i < result.length; i++) {
+                       var id = result[i].id;
+                       var name = result[i].name;
+                       var button = $('<button class="btn btn-success">Save</button>')                
+                       var phone = result[i].phone; 
+                       var website = result[i].website_url;
+                       var newItem = $("<li>").text(name);
+                       var phoneP = $("<p>").text(phone);
+                       var webP = $("<p>").text(website);
+                       newItem.append(phoneP);
+                       newItem.append(webP);
+                       newItem.append(button);
+                       list.append(newItem);
+                       $(".results").append(list);
+                     }
+             })
+    })
+    })
 
 })
